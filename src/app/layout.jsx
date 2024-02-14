919e8859b57190194c7f9772f467b52f05536dac
bbeck9123 <byron.ptgo@gmail.com>
2024-02-14 15:16:04 -0800

    initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Byron Beckers',
+    default:
+      'Byron Becker - Software Engineercan.',
+  },
+  description:
+    'I’m Byron, a software engineer and I build exceptional digital experiences for those who have a vision for an online platform.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
